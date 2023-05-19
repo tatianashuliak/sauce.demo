f61b868a2c9c0b35df0e68f6b7274bc3756ee36a
Tatiana Shuliak <tatjananikolaevich23041997@gmail.com>
2023-05-19 22:42:48 +0300

    Saucedemo test added

---
+++ src/test/java/constants/PageUrls.java
@@ -0,0 +1,5 @@
+package constants;
+
+public class PageUrls {
+    public static final String LOGIN_PAGE = "https://www.saucedemo.com/";
+}
---
+++ src/test/java/tests/BaseTest.java
@@ -0,0 +1,26 @@
+package tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.util.concurrent.TimeUnit;
+
+public class BaseTest {
+    protected WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void tearDown() {
+        driver.quit();
+    }
+
+}
